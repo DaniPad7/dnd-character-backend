50468b31d7c6d85788a0bf2a55f91a6f7fcd2180
KeithSantamaria <36080275+KeithSantamaria@users.noreply.github.com>
2021-07-22 17:20:12 -0500

    got createCharacter ready for post request

--- frontend/src/components/CreateCharacterContainer.js
+++ frontend/src/components/CreateCharacterContainer.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line 
 import React, {useState} from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import CreateCharacterForm from '../forms/CreateCharacterForm';
@@ -5,10 +6,34 @@ import CreateCharacterForm from '../forms/CreateCharacterForm';
 const CreateCharacterContainer = () => {
   const [show, setShow] = useState(false);
 
+  const [name ,setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [str, setStr] = useState(1);
+  const [dex, setDex] = useState(1);
+  const [con, setCon] = useState(1);
+  const [int, setInt] = useState(1);
+  const [wis, setWis] = useState(1);
+  const [cha, setCha] = useState(1);
+  const [alignment, setAlignment] = useState("");
+  const [faction, setFaction] = useState("")
+
   const handleShow = () => setShow(true);
   
   const handleClose = () => {
     setShow(false);
+
+    const payload = {
+      name : name,
+      desc : description ,
+      statStrength : str ,
+      statDexerity : dex ,
+      statConstitution : con ,
+      statIntelligence : int ,
+      statWisdom : wis ,
+      statCharisma : cha,
+      faction : faction,
+      alignment : alignment
+    }
   }
 
   const handleCloseRandom = () => {
@@ -26,7 +51,18 @@ const CreateCharacterContainer = () => {
           <Modal.Title> Create Your Character here</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <CreateCharacterForm/>
+          <CreateCharacterForm
+            setName = {setName}
+            setDescription = {setDescription}
+            setStr = {setStr}
+            setDex = {setDex}
+            setCon = {setCon}
+            setInt = {setInt}
+            setWis = {setWis}
+            setCha = {setCha}
+            setAlignment = {setAlignment}
+            setFaction = {setFaction}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button variant = "primary" onClick = {handleClose}>
--- frontend/src/forms/CreateCharacterForm.js
+++ frontend/src/forms/CreateCharacterForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Form, InputGroup} from 'react-bootstrap';
 import * as Alignments from '../constants/alignment';
 
@@ -13,12 +13,12 @@ const CreateCharacterForm = (props)=> {
         
         <Form.Group className = "characterCreate" controlId= "characterName">
           <Form.Label>Character Name</Form.Label>
-          <Form.Control type="text" placeholder= "Enter Name here" />
+          <Form.Control type="text" placeholder= "Enter Name here" onChange = { (e) => {props.setName(e.target.value)}}/>
         </Form.Group>
 
         <Form.Group className = "characterCreate" controlId = "characterDescription">
           <Form.Label>Character Description</Form.Label>
-          <Form.Control as = "textarea" placeholder = "Enter Description here" />
+          <Form.Control as = "textarea" placeholder = "Enter Description here" onChange = { (e) => {props.setDescription(e.target.value)}}/>
         </Form.Group>
 
         <br/>
@@ -26,11 +26,11 @@ const CreateCharacterForm = (props)=> {
         <Form.Group className = "characterCreate" controlId = "characterStat">
           <InputGroup className = "characterCreateStats" >
             <Form.Label>STR </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/> 
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setStr(e.target.value)} }/> 
             <Form.Label>DEX </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/>  
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setDex(e.target.value)} }/>  
             <Form.Label>CON </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/>  
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setCon(e.target.value)} }/>  
           </InputGroup>    
         </Form.Group>
 
@@ -39,31 +39,31 @@ const CreateCharacterForm = (props)=> {
         <Form.Group className = "characterCreate" controlId = "characterStat2">
           <InputGroup className = "characterCreateStats" >
             <Form.Label>INT </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/>  
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setInt(e.target.value)} }/>  
             <Form.Label>WIS </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/>  
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setWis(e.target.value)} }/>  
             <Form.Label>CHA </Form.Label>
-            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT}/>
+            <Form.Control type="number" min = {MIN_STAT} max = {MAX_STAT} onChange = { (e) => {props.setCha(e.target.value)} }/>
             </InputGroup>    
         </Form.Group>
 
-        <Form.Group className = "characterCreate" controlId = "characterAlignment">
+        <Form.Group className = "characterCreate" controlId = "characterAlignment" >
           <Form.Label> Character Alignment : </Form.Label>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'lg' label = {Alignments.LawfulGood}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'ng' label = {Alignments.NeutralGood}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'cg' label = {Alignments.ChaoticGood}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'ln' label = {Alignments.LawfulNeutral}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'n'  label = {Alignments.Neutral}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'cn' label = {Alignments.ChaoticNeutral}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'le' label = {Alignments.LawfulEvil}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'ne' label = {Alignments.NeutralEvil}/>
-          <Form.Check inline type = "radio"  name="Alignment" id= 'ce' label = {Alignments.ChaoticEvil}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'lg' label = {Alignments.LawfulGood} onChange = { () => {props.setAlignment(Alignments.LawfulGood)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'ng' label = {Alignments.NeutralGood} onChange = { () => {props.setAlignment(Alignments.NeutralGood)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'cg' label = {Alignments.ChaoticGood} onChange = { () => {props.setAlignment(Alignments.ChaoticGood)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'ln' label = {Alignments.LawfulNeutral} onChange = { () => {props.setAlignment(Alignments.LawfulNeutral)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'n'  label = {Alignments.Neutral} onChange = { () => {props.setAlignment(Alignments.Neutral)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'cn' label = {Alignments.ChaoticNeutral} onChange = { () => {props.setAlignment(Alignments.ChaoticNeutral)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'le' label = {Alignments.LawfulEvil} onChange = { () => {props.setAlignment(Alignments.LawfulEvil)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'ne' label = {Alignments.NeutralEvil} onChange = { () => {props.setAlignment(Alignments.NeutralEvil)}}/>
+          <Form.Check inline type = "radio"  name="Alignment" id= 'ce' label = {Alignments.ChaoticEvil} onChange = { () => {props.setAlignment(Alignments.ChaoticEvil)}}/>
         </Form.Group>
 
 
         <Form.Group className = "characterCreate" controlId = "characterFaction">
           <Form.Label> Character Faction :</Form.Label>
-          <Form.Control type="text" placeholder= "Enter Faction here" />
+          <Form.Control type="text" placeholder= "Enter Faction here" onChange = { (e) => {props.setFaction(e.target.value)} }/>
          </Form.Group>
 
       </Form>
