3be1279ff895f2005ca6daa836fbb5099058c5e5
Deusiratus <rstaylor971@gmail.com>
2021-07-22 15:41:39 -0400

    save point

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import CreateCharacterContainer from './components/CreateCharacterContainer';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Container fluid>
         <Row >
           <p>Character Cards goes here</p>
@@ -13,6 +15,7 @@ function App() {
           <CreateCharacterContainer/>
         </Row>
       </Container>
+      </Router>
     </div>
   );
 }
