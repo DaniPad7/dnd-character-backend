df0f27d59deec5805ac457f2e4d00ca599a1e534
DaniPad7 <padilladaniel777@gmail.com>
2021-07-22 09:37:57 -0700

    Structure added

---
+++ DnD-Character/src/main/java/com/revature/dnd/DnDCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.revature.dnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDCharacterApplication.class, args);
+	}
+
+}
---
+++ DnD-Character/src/test/java/com/revature/dnd/DnDCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.dnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDCharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
